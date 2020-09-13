4b03fbb24eb3700c3c5311fcea4ffbb0c987ba9b
ww <ww@qq.com>
2020-09-14 00:04:01 +0800

    add udp

--- tcp.py
+++ tcp.py
@@ -6,29 +6,23 @@ from asyncio_pool import AioPool
 from scapy.sendrecv import sr1, send, srp, srp1, sr
 from scapy.layers.inet import IP, ICMP, TCP
 
-def tcp():
+def tcp_task():
     #随机产生一个1-65535的IP的id位
     ip_id=randint(1,65535)
-    #随机产生一个1-65535的icmp的id位
-    icmp_id=randint(1,65535)
-    #随机产生一个1-65535的icmp的序列号
-    icmp_seq=randint(1,65535)
-    pkt = IP(dst='192.168.214.1', ttl=64, id=ip_id) / ICMP(id=icmp_id, seq=icmp_seq)
+    data = "m"*1400  # 一个最大传输单元（MTU）为1500字节，减去ip头20字节，再减去tcp头20字节，最大可传输数据为1460字节
+    pkt = IP(dst='192.168.214.129', id=ip_id) / TCP(dport=8080, flags="S")/data.encode(encoding="utf-8")
     #  发送包，超时时间为1秒，如果对端没有响应，则返回None，有响应则返回响应包
     res = sr1(pkt, timeout=1, verbose=False)
     return res
 
-
 async def  tcp_worker():
     loop = asyncio.get_running_loop()
-    res = await loop.run_in_executor(None, ping)
+    res = await loop.run_in_executor(None, tcp_task)
     if res:
-        icmp = res[1][0]["ICMP"]
-        echo_reply = icmp.type  # type=0: ping应答成功
-        print(echo_reply, "echo")
-        # s = res[1][0]["ICMP"].show()
-        # print(s)
-    print(res)
+        # tcp = res.show()
+        flags = res[1].flags #  flags为RA（RST/ACK）表示目标主机存活且探测的端口未开放
+                                     #  flags为SA（SYN/ACK）表示目标主机存活且探测的端口开放
+        print(flags)
     return res
 
 async def spawn(work_num, size):
@@ -39,8 +33,8 @@ async def spawn(work_num, size):
     futures = []
     async with AioPool(size=size) as pool:
         for i in range(work_num):
-            f = await pool.spawn(ping_worker())
+            f = await pool.spawn(tcp_worker())
             futures.append(f)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(asyncio.gather(spawn(1000000000000, 50000)))
+    asyncio.get_event_loop().run_until_complete(asyncio.gather(spawn(1000000000000, 5000)))
--- udp.py
+++ udp.py
@@ -1,33 +1,28 @@
-# -*- coding: UTF-8 -*-
+
 from random import randint
 
 import asyncio
 from asyncio_pool import AioPool
 from scapy.sendrecv import sr1, send, srp, srp1, sr
-from scapy.layers.inet import IP, ICMP
+from scapy.layers.inet import IP, ICMP, TCP,UDP
 
-def ping():
+def udp_task():
     #随机产生一个1-65535的IP的id位
     ip_id=randint(1,65535)
     #随机产生一个1-65535的icmp的id位
     icmp_id=randint(1,65535)
     #随机产生一个1-65535的icmp的序列号
     icmp_seq=randint(1,65535)
-    pkt = IP(dst='192.168.214.129', ttl=64, id=ip_id) / ICMP(id=icmp_id, seq=icmp_seq)
+    pkt = IP(dst='192.168.214.129') / UDP(dport="53")
     #  发送包，超时时间为1秒，如果对端没有响应，则返回None，有响应则返回响应包
-    res = sr1(pkt, timeout=1, verbose=False)
+    res = sr1( pkt, timeout=1, verbose=False )/b"hello"
+    # res = sr1(pkt, timeout=1, verbose=False)
     return res
 
 
-async def  ping_worker():
+async def  udp_worker():
     loop = asyncio.get_running_loop()
-    res = await loop.run_in_executor(None, ping)
-    if res:
-        icmp = res[1][0]["ICMP"]
-        echo_reply = icmp.type  # type=0: ping应答成功
-        print(echo_reply, "echo")
-        # s = res[1][0]["ICMP"].show()
-        # print(s)
+    res = await loop.run_in_executor(None, udp_task)
     print(res)
     return res
 
@@ -39,8 +34,8 @@ async def spawn(work_num, size):
     futures = []
     async with AioPool(size=size) as pool:
         for i in range(work_num):
-            f = await pool.spawn(ping_worker())
+            f = await pool.spawn(udp_worker())
             futures.append(f)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(asyncio.gather(spawn(1000000000000, 500)))
+    asyncio.get_event_loop().run_until_complete(asyncio.gather(spawn(1000000000000, 50)))
